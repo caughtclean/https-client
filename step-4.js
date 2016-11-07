c27dbeee4c847c3a72a90dcef0e3a86bea4828f7
Nathan Eyerley <eyerleyn@gmail.com>
2016-11-07 21:24:43 +0000

    added step 4

---
+++ step-4.js
@@ -0,0 +1,35 @@
+var https = require('https');
+
+
+function getHTML(options, callback) {
+  var buffer = {}
+  var obj = options
+  https.get(obj, function(response) {
+    response.setEncoding('utf8');
+    response.on('data', function(chunk){
+      buffer += chunk
+
+    });
+    response.on('end', function() {
+      printHTML(buffer)
+      console.log('end');
+
+
+    });
+  });
+
+}
+
+function printHTML(html) {
+  console.log(html);
+}
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html',
+  };
+
+
+
+
+getHTML(requestOptions, printHTML)
